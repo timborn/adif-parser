09fa2ff88e32dd890071c7d80894a7997e7755ec
timborn <Timothy.D.Born@gmail.com>
2023-01-09 16:45:35 -0700

    expand on basic parsing of ADIF from LoTW. DEFINITELY depends on adif-json-protobuf for the heavy lifting here. TODO: that's a hard-coded dependency that needs fixing

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	./contacted-stations.sh  ~/Downloads/*adi
--- contacted-stations.sh
+++ contacted-stations.sh
@@ -1,3 +1,19 @@
 # given an ADIF, list of contacted stations in JSON format
 # TODO: the input ADIF is hard-coded
-## YES ## python3 demo.py ~/Downloads/*adi 2>/dev/null | jq -c '.qsos[] | { contactedStation: .contactedStation.stationCall } '
+# NB depends on adif-json-protobuf (github)
+
+if [ $# -ne 1 ] ; then
+	echo "Usage: $0 <contact.adi>"
+	exit 1
+fi
+IFILE=$1
+
+python3 ../adif-json-protobuf/demo/demo.py $IFILE 2>/dev/null | 
+	jq -c '.qsos[] | { 
+		contactedStation: .contactedStation.stationCall,
+		band: .band,
+		freq: .freq,
+		mode: .mode,
+		dateTime: .timeOn
+		} '
+
