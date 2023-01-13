e985d5fb5959e5bc3a3310deb8e4c830a8f73caf
timborn <Timothy.D.Born@gmail.com>
2023-01-12 19:19:19 -0700

    prefix-to-country works on mac now

--- callsign-list.sh
+++ callsign-list.sh
@@ -1,4 +1,4 @@
 # sorted list of contacted station callsigns from a given ADIF
-# TODO: input ADIF is hard-coded
 # NB the list of callsigns shows up enclosed in double quotes
-python3 demo.py ~/Downloads/*adi 2>/dev/null | jq -c '.qsos[] |  .contactedStation.stationCall  ' | sort -u 
+DEMO=/Volumes/BlueMountain/Users/timborn/code/ham/adif-parser/adif-json-protobuf/demo/demo.py
+python3 $DEMO ~/Downloads/lotwreport.adi 2>/dev/null | jq -c '.qsos[] |  .contactedStation.stationCall  ' | sort -u 
