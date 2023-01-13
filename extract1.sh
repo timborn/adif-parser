2e69bfd3552eee4259776fef5390f0e4714f6ca6
timborn <Timothy.D.Born@gmail.com>
2023-01-12 19:20:51 -0700

    fixed hard-coded stuff in extract1.sh

--- extract1.sh
+++ extract1.sh
@@ -4,4 +4,7 @@
 # http://www.arrl.org/files/file/LoTW%20Instructions/LoTW%20User%20Account/Your%20QSOS.pdf
 # "This report downloads an ADIF file of confirmed QSOs."
 # THEREFORE if you download the report from LoTW you get only QSLs
-python3 demo.py ~/Downloads/*adi 2>/dev/null | jq -c '.qsos[] | { contactedStation: .contactedStation.stationCall, band: .band, freq: .freq, mode: .mode, dateTime: .timeOn  } '
+# TODO: fix hard-coded logbook
+
+DEMO="$HOME/code/ham/adif-parser/adif-json-protobuf/demo/demo.py"
+python3 $DEMO demo.py ~/Downloads/lotw.adi 2>/dev/null | jq -c '.qsos[] | { contactedStation: .contactedStation.stationCall, band: .band, freq: .freq, mode: .mode, dateTime: .timeOn  } '
